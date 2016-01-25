0975bd2c76c030adb8deb72d72c466f4f58a1555
Jonathan Roster <jroster76@gmail.com>
2016-01-24 22:23:27 -0800

    user input for testing

--- src/main.cpp
+++ src/main.cpp
@@ -1,4 +1,4 @@
-#include <iostream>
+#include <cstdio>
 
 //include the opencv libraries
 #include <cv.h>
@@ -9,23 +9,25 @@
 using namespace std;
 using namespace cv;
 
+int n_boards = 0;
+int board_w;
+int board_h;
+int corner_count;
+int successes = 0;
+
 int main(int argc, const char** argv)
 {
-	Mat img = imread("res/left01.jpg", CV_LOAD_IMAGE_UNCHANGED); //read the image data in the file "MyPic.JPG" and store it in 'img'
+	char str[15], ext[15];
 
-	if (img.empty()) //check whether the image is loaded or not
-	{
-		cout << "Error : Image cannot be loaded..!!" << endl;
-		//system("pause"); //wait for a key press
-		return -1;
-	}
-
-	namedWindow("MyWindow", CV_WINDOW_AUTOSIZE); //create a window with the name "MyWindow"
-	imshow("MyWindow", img); //display the image which is stored in the 'img' in the "MyWindow" window
-
-	waitKey(0); //wait infinite time for a keypress
-
-	destroyWindow("MyWindow"); //destroy the window with the name, "MyWindow"
-
-	return 0;
+	int board_h = 0, board_w = 0;
+	printf("Enter the name of the Images (String) without numeric denominator:  ");
+	scanf("%s", str);
+	printf("Enter the extension of the Images (String):  ");
+	scanf("%s", ext);
+	printf("Enter the total number of the File (should be integer) starting with index << 1 >> :: ");
+	scanf("%d", &n_boards);
+	printf("Enter the the board width (should be integer) :: ");
+	scanf("%d", &board_w);
+	printf("Enter the the board height (should be integer) :: ");
+	scanf("%d", &board_h);
 }
