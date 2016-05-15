12519e5ee2c12f8336af0659561c8af97b9e03a5
Jonathan Roster <jroster76@gmail.com>
2016-05-15 16:44:40 -0700

    dsafa

--- src/stereo_calibrate.cpp
+++ src/stereo_calibrate.cpp
@@ -315,8 +315,8 @@ void Init_SBM(){
 	//bm->setNumDisparities(64); //number of disparities
 	//bm->setTextureThreshold(10);
 //	bm->setUniquenessRatio(15);
-	bm->setSpeckleWindowSize(32);
-	bm->setSpeckleRange(8);
+	bm->setSpeckleWindowSize(16);
+	bm->setSpeckleRange(4);
 //	bm->setDisp12MaxDiff(1);
 
 }
