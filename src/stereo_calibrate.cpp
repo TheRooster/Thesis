4bbd0e41ebaf4d94aa1e7604497cb0bb359e3e8e
Jonathan Roster <jroster76@gmail.com>
2016-05-15 17:20:28 -0700

    dsfds

--- src/stereo_calibrate.cpp
+++ src/stereo_calibrate.cpp
@@ -312,10 +312,10 @@ void Init_SBM(){
 	bm->setPreFilterCap(31);
 	//bm->setBlockSize(9); //block size to check
 //	bm->setMinDisparity(-32);
-	bm->setNumDisparities(128); //number of disparities
+	bm->setNumDisparities(96); //number of disparities
 	//bm->setTextureThreshold(10);
 //	bm->setUniquenessRatio(15);
-	bm->setSpeckleWindowSize(96);
+	bm->setSpeckleWindowSize(64);
 	bm->setSpeckleRange(128);
 //	bm->setDisp12MaxDiff(1);
 
